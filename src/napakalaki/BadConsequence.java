6fc64c2a9a59285b02fbd05bccbb788476ac3cb8
rafa <rafaelleru95103@gmail.com>
2016-10-04 18:17:45 +0200

    1ª version BadConsequence. añadido metodo toString

---
+++ src/napakalaki/BadConsequence.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rafa
+ */
+public class BadConsequence {
+    private String text;
+    private int levels;
+    private int nVisibleTreasures;
+    private int nHiddenTreasures;
+    private boolean death;
+    private ArrayList<TreasureKind> specificHiddenTreasures = new ArrayList<>();
+    private ArrayList<TreasureKind> specificVisibleTreasures = new ArrayList<>();
+    
+    /**
+     * 
+     * @param t texto 
+     * @param l levels
+     * @param nV nVisibleTreasures
+     * @param nH  nHiddenTreasures
+     */
+    public BadConsequence(String t, int l, int nV, int nH){
+        text = t;
+        levels = l;
+        nVisibleTreasures = nV;
+        nHiddenTreasures = nH;
+    }
+    
+    /**
+     * 
+     * @param t texto
+     * @param d valor que define si hay muerte o no
+     */
+    public BadConsequence(String t, boolean d){
+        text = t;
+        death = d;
+    }
+    
+    /**
+     * 
+     * @param text texto
+     * @param levels niveles
+     * @param tVisible array de tipos de los tesoros visibles
+     * @param tHidden array de tipos de los tesoros ocultos, 
+     */
+    public BadConsequence(String text, int levels, ArrayList<TreasureKind> tVisible,
+            ArrayList<TreasureKind> tHidden){
+        text = text;
+        levels = levels;
+        specificHiddenTreasures = tHidden;
+        specificVisibleTreasures = tVisible;
+    }
+    
+    public String toString(){
+        return text + " decrementa: " + Integer.toString(levels);
+    }
+
+}
