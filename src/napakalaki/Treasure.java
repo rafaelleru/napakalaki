a698aea195480735287e923186e82115972e1f1e
archdri <archdri@archdri>
2016-11-04 14:05:11 +0100

    Realizada clase Treasure con cabeceras de sus metodos

---
+++ src/napakalaki/Treasure.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author archdri
+ */
+public class Treasure {
+    
+    private String name;
+    private int bonus;
+    
+    public Treasure(String n, int bonus, TreasureKind t){}
+    public String getName(){}
+    public int getBonus(){}
+    public TreasureKind getType(){}
+}
