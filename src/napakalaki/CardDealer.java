634c1536e948e7bd1fdf3c7d81342cbfade68d6d
archdri <archdri@archdri>
2016-11-04 13:59:38 +0100

    Realizada clase CardDealer

---
+++ src/napakalaki/CardDealer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author archdri
+ */
+public class CardDealer {
+    
+    private static CardDealer = null;
+    
+    private CardDealer(){}
+    private void initTreasuresCardDeck(){}
+    private void initMonsterCardDeck(){}
+    private void shuffleTreasures(){}
+    private void shuffleMonsters(){}
+    public static CardDealer getInstance(){}
+    public Treasure nextTreasure(){}
+    public Monster nextMonster(){}
+    public void giveTreasureBack(Treasure t){}
+    public void giveMonsterBack(Monster m){}
+    public void initCards(){}
+    
+}
