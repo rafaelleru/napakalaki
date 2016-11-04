75f63abf9de2ff8fdb9e32b1ba8eec912c3aaecd
archdri <archdri@archdri>
2016-11-04 13:51:54 +0100

    Realizada clase Dice

---
+++ src/napakalaki/Dice.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author archdri
+ */
+public class Dice {
+    
+    private static Dice instance = null;
+    
+    private Dice(){}
+    public static Dice getInstance(){}
+    public int nextNumber(){}
+    
+}
